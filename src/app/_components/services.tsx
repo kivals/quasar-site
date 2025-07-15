f6ef2d18841f5b262807981121ccd07e5416e0b7
vvmelnik <vvmelnik@quasarpro.ru>
2025-07-15 11:25:50 +0300

    Products and Services. adaptive fixes

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,44 +1,51 @@
 import { PropsWithChildren } from "react";
 import { ServicesBackground } from "./services-background";
+import Container from "@/components/container";
+import { Button } from "@/components/ui/button";
 
 function ServiceItem({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col justify-center rounded-[30px] bg-white/80 px-3 py-4 backdrop-blur-3xl max-xl:items-center max-xl:text-center xl:justify-end xl:px-11 xl:py-15">
-      <p className="text-[24px] xl:text-[40px]">{children}</p>
-      <a className="text-accent mt-2.5 text-[24px] font-semibold xl:text-[32px]">
+    <div className="flex flex-col rounded-[30px] bg-white/80 px-3 py-4 backdrop-blur-3xl max-xl:items-center max-xl:text-center xl:justify-end xl:px-8 xl:py-10 2xl:px-11 2xl:py-15">
+      <p className="text-[24px] xl:text-[32px] 2xl:text-[40px]">{children}</p>
+      <Button
+        variant="link"
+        className="text-accent mt-5 cursor-pointer justify-start pl-0 font-semibold md:text-[20px] lg:text-[24px] xl:text-[32px]"
+      >
         Подробнее
-      </a>
+      </Button>
     </div>
   );
 }
 
 export function Services() {
   return (
-    <div className="relative mt-8.5 xl:mx-21.5 xl:mt-30">
-      <h2 className="text-center text-[28px] font-medium xl:text-[80px]">
-        Услуги
-      </h2>
+    <Container>
+      <div className="relative mt-8.5 xl:mt-30">
+        <h2 className="text-center text-[28px] font-medium xl:text-[40px] 2xl:text-[80px]">
+          Услуги
+        </h2>
 
-      <div className="mt-5 grid grid-rows-2 gap-2.5 max-xl:mx-5 xl:grid-rows-[auto_auto] xl:gap-7.5 xl:drop-shadow-[0_0_50px_#00000020]">
-        <div className="grid grid-rows-2 gap-2.5 xl:grid-cols-2 xl:grid-rows-1 xl:gap-7.5 xl:pr-26">
-          <ServiceItem>
-            Разработка ИТ-систем: проектирование, разработка и внедрение
-            программного обеспечения под задачи бизнеса
-          </ServiceItem>
-          <ServiceItem>Сопровождение информационных систем</ServiceItem>
+        <div className="mt-5 grid grid-rows-2 gap-2.5 max-xl:mx-5 xl:grid-rows-[auto_auto] xl:gap-7.5 xl:drop-shadow-[0_0_50px_#00000020]">
+          <div className="grid grid-rows-2 gap-2.5 xl:grid-cols-2 xl:grid-rows-1 xl:gap-5 2xl:gap-7.5 xl:pr-26">
+            <ServiceItem>
+              Разработка ИТ-систем: проектирование, разработка и внедрение
+              программного обеспечения под задачи бизнеса
+            </ServiceItem>
+            <ServiceItem>Сопровождение информационных систем</ServiceItem>
+          </div>
+
+          <div className="grid grid-rows-2 gap-2.5 xl:grid-cols-2 xl:grid-rows-1 xl:gap-7.5 xl:pl-26">
+            <ServiceItem>Разработка встраиваемых систем</ServiceItem>
+            <ServiceItem>Разработка сайтов</ServiceItem>
+          </div>
         </div>
 
-        <div className="grid grid-rows-2 gap-2.5 xl:grid-cols-2 xl:grid-rows-1 xl:gap-7.5 xl:pl-26">
-          <ServiceItem>Разработка встраиваемых систем</ServiceItem>
-          <ServiceItem>Разработка сайтов</ServiceItem>
+        <div className="absolute inset-0 -z-10 overflow-clip xl:hidden">
+          <div className="absolute -top-62 -left-80">
+            <ServicesBackground />
+          </div>
         </div>
       </div>
-
-      <div className="absolute inset-0 -z-10 overflow-clip xl:hidden">
-        <div className="absolute -top-62 -left-80">
-          <ServicesBackground />
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 }
--- src/app/_components/trusted.tsx
+++ src/app/_components/trusted.tsx
@@ -5,7 +5,7 @@ export function Trusted() {
   return (
     <section className="mt-8.5 2xl:mt-19">
       <Container>
-        <h2 className="text-center text-[28px] font-medium lg:mb-4 lg:text-[40px] xl:mb-6 xl:text-[60px] 2xl:mb-8 2xl:text-[80px]">
+        <h2 className="text-center text-[28px] font-medium lg:mb-4 lg:text-[40px] mb-4 xl:mb-6 xl:text-[60px] 2xl:mb-8 2xl:text-[80px]">
           Нам доверяют
         </h2>
 
